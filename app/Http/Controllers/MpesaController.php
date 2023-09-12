e323de5af8bd5ac0acd46d1480d1b75680ada718
okonueugene <tylarrex67@gmail.com>
2023-09-12 12:38:23 +0300

    changes for database

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Mpesa;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 
 class MpesaController extends Controller
@@ -16,6 +18,8 @@ class MpesaController extends Controller
         $response = Http::withBasicAuth(env('CONSUMER_KEY'), env('CONSUMER_SECRET'))->get(env('GENERATE_ACCESS_TOKEN_URL'));
 
         $access_token = json_decode($response->getBody());
+
+
 
         return $access_token->access_token;
 
@@ -46,11 +50,11 @@ class MpesaController extends Controller
             'Password' => base64_encode(env('STORE_NUMBER').env('PASSKEY').date("YmdHis")),
             'Timestamp' => date("YmdHis"),
             'TransactionType' => 'CustomerPayBillOnline',
-            'Amount' => '1',
+            'Amount' => '100000',
             'PartyA' => env('PHONE_NUMBER'),
             'PartyB' => env('STORE_NUMBER'),
             'PhoneNumber' => env('PHONE_NUMBER'),
-            'CallBackURL' => env('CALLBACK_URL'),
+            'CallBackURL' => "https://api.optitech.co.ke/api/deliverance/confirmation",
             'AccountReference' => "Test",
             'TransactionDesc' => "Test"
         );
@@ -63,7 +67,9 @@ class MpesaController extends Controller
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 
         $curl_response = curl_exec($curl);
-        echo $curl_response;
+
+        //return json response
+        return $curl_response;
 
     }
 
@@ -74,23 +80,52 @@ class MpesaController extends Controller
 
         if($content->Body->stkCallback->ResultCode == 0) {
 
-            $mpesa_transaction = new Mpesa();
-            $mpesa_transaction->MerchantRequestID = $content->Body->stkCallback->MerchantRequestID;
-            $mpesa_transaction->CheckoutRequestID = $content->Body->stkCallback->CheckoutRequestID;
-            $mpesa_transaction->ResultCode = $content->Body->stkCallback->ResultCode;
-            $mpesa_transaction->ResultDesc = $content->Body->stkCallback->ResultDesc;
-            $mpesa_transaction->Amount = $content->Body->stkCallback->CallbackMetadata->Item[0]->Value;
-            $mpesa_transaction->MpesaReceiptNumber = $content->Body->stkCallback->CallbackMetadata->Item[1]->Value;
-            $mpesa_transaction->TransactionDate = (string)$content->Body->stkCallback->CallbackMetadata->Item[2]->Value;
-            $mpesa_transaction->PhoneNumber = (string)$content->Body->stkCallback->CallbackMetadata->Item[3]->Value;
-            $mpesa_transaction->save();
+            // $mpesa_transaction = new Mpesa();
+            // $mpesa_transaction->MerchantRequestID = $content->Body->stkCallback->MerchantRequestID;
+            // $mpesa_transaction->CheckoutRequestID = $content->Body->stkCallback->CheckoutRequestID;
+            // $mpesa_transaction->ResultCode = $content->Body->stkCallback->ResultCode;
+            // $mpesa_transaction->ResultDesc = $content->Body->stkCallback->ResultDesc;
+            // $mpesa_transaction->Amount = $content->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+            // $mpesa_transaction->MpesaReceiptNumber = $content->Body->stkCallback->CallbackMetadata->Item[1]->Value;
+            // $mpesa_transaction->TransactionDate = (string)$content->Body->stkCallback->CallbackMetadata->Item[2]->Value;
+            // $mpesa_transaction->PhoneNumber = (string)$content->Body->stkCallback->CallbackMetadata->Item[3]->Value;
+            // $mpesa_transaction->created_at = Carbon::now();
+            // $mpesa_transaction->updated_at = Carbon::now();
+            // $mpesa_transaction->save();
+
+            $mpesa_transaction = DB::table('mpesa_transactions')->insert([
+                'MerchantRequestID' => $content->Body->stkCallback->MerchantRequestID,
+                'CheckoutRequestID' => $content->Body->stkCallback->CheckoutRequestID,
+                'ResultCode' => $content->Body->stkCallback->ResultCode,
+                'ResultDesc' => $content->Body->stkCallback->ResultDesc,
+                'Amount' => $content->Body->stkCallback->CallbackMetadata->Item[0]->Value,
+                'MpesaReceiptNumber' => $content->Body->stkCallback->CallbackMetadata->Item[1]->Value,
+                'TransactionDate' => (string)$content->Body->stkCallback->CallbackMetadata->Item[2]->Value,
+                'PhoneNumber' => (string)$content->Body->stkCallback->CallbackMetadata->Item[3]->Value,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+
+            ]);
         } else {
-            $mpesa_transaction = new Mpesa();
-            $mpesa_transaction->MerchantRequestID = $content->Body->stkCallback->MerchantRequestID;
-            $mpesa_transaction->CheckoutRequestID = $content->Body->stkCallback->CheckoutRequestID;
-            $mpesa_transaction->ResultCode = $content->Body->stkCallback->ResultCode;
-            $mpesa_transaction->ResultDesc = $content->Body->stkCallback->ResultDesc;
-            $mpesa_transaction->save();
+            // $mpesa_transaction = new Mpesa();
+            // $mpesa_transaction->MerchantRequestID = $content->Body->stkCallback->MerchantRequestID;
+            // $mpesa_transaction->CheckoutRequestID = $content->Body->stkCallback->CheckoutRequestID;
+            // $mpesa_transaction->ResultCode = $content->Body->stkCallback->ResultCode;
+            // $mpesa_transaction->ResultDesc = $content->Body->stkCallback->ResultDesc;
+            // //created_at
+            // //updated_at
+            // $mpesa_transaction->created_at = Carbon::now();
+            // $mpesa_transaction->updated_at = Carbon::now();
+            // $mpesa_transaction->save();
+
+            $mpesa_transaction = DB::table('mpesa_transactions')->insert([
+                'MerchantRequestID' => $content->Body->stkCallback->MerchantRequestID,
+                'CheckoutRequestID' => $content->Body->stkCallback->CheckoutRequestID,
+                'ResultCode' => $content->Body->stkCallback->ResultCode,
+                'ResultDesc' => $content->Body->stkCallback->ResultDesc,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
 
         }
 
